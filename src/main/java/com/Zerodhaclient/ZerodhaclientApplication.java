f303bb5b37d0ab4e2da0dbb6d287f38ec4a0d020
Kumar, Nikhil(ak456s) <ak456s@att.com>
2020-05-29 09:57:01 +0530

    intial sample code

---
+++ src/main/java/com/Zerodhaclient/ZerodhaclientApplication.java
@@ -0,0 +1,13 @@
+package com.Zerodhaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerodhaclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerodhaclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Zerodhaclient/ZerodhaclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zerodhaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerodhaclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
